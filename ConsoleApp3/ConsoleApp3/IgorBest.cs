f80d8a822022b3edb86edfc665a95e7111d79d14
MotryaVasya <motravasa5@gmail.com>
2024-11-07 19:48:31 +0700

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/IgorBest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class IgorBest
+    {
+        public static async Task<string> GetS()
+        {
+            WebClient client = new WebClient();
+            client.DownloadFile("https://d12.drivemusic.me/dl/HphW6uCD_jLe5gWRJN9SJA/1730224299/download_music/2020/06/julija-chicherina-tu-lu-la.mp3", "C:\\Users\\STUDENT\\вася");
+            HttpClient httpClient = new HttpClient();
+            string r = await httpClient.GetStringAsync("https://www.drivemusic.club/song/235626-julija-chicherina-tu-lu-la.html");
+            return r;
+        }
+
+        public async void DoSomethingAsync()
+        {
+            Console.Write("x=");
+            double x = await DoSomething(200);
+            Console.WriteLine(x);
+
+            Console.Write("y=");
+            double y = await DoSomething(100);
+            Console.WriteLine(y);
+        }
+        public Task<double> DoSomething(int n)
+        {
+            return Task.Run(() =>
+            {
+                double s = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        for (int k = 0; k < n; k++)
+                        {
+                            s += (Math.Sqrt(Math.E * i) * Math.PI) / (k + 1);
+                        }
+                    }
+                }
+                return s;
+            });
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        public static async Task<string> GetS()
+        {
+            WebClient client = new WebClient();
+            client.DownloadFile("https://d12.drivemusic.me/dl/HphW6uCD_jLe5gWRJN9SJA/1730224299/download_music/2020/06/julija-chicherina-tu-lu-la.mp3", "C:\\Users\\TEACHER\\вася");
+            HttpClient httpClient = new HttpClient();
+            string r = await httpClient.GetStringAsync("https://www.drivemusic.club/song/235626-julija-chicherina-tu-lu-la.html");
+            return r;
+        }
+        static void Main(string[] args)
+        {
+            IgorBest igorBest = new IgorBest();
+            Task<string> t = GetS();
+            Console.WriteLine(IgorBest.GetS());
+            Console.ReadLine();
+        }
+    }
+}
